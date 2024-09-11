0c5b3ce6e0ea0c3eb7b6916b6953ac9c69c74129
Toby Wise <tobyw31@gmail.com>
2024-09-11 21:27:54 +0100

    Create firebaseConfig.js

---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "your-api-key",
+  authDomain: "your-auth-domain",
+  projectId: "your-project-id",
+  storageBucket: "your-storage-bucket",
+  messagingSenderId: "your-messaging-sender-id",
+  appId: "your-app-id",
+};
+  
+export default firebaseConfig;
